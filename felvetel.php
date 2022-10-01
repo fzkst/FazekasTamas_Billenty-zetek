7a73a176ba104e93f3dfe3c9bf9651118a7347aa
Fazekas Tamás <tamas.fzks@gmail.com>
2022-10-01 17:17:39 +0200

    táblázat létrehozása, Bootstrap formázás

--- felvetel.php
+++ felvetel.php
@@ -7,46 +7,54 @@
     <title>Billentyűzetek</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 
 </head>
 <body>
-    <nav class="nav nav-pills flex-column flex-sm-row">
-        <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Listázás</a>
-        <a class="flex-sm-fill text-sm-center nav-link active" href="felvetel.php">Felvétel</a>  
+    <nav class="ms-4">
+        <ul class="nav my-3 mx-5 nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Felvétel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Listázás</a>
+            </li>   
+        </ul>
     </nav>
+
     <main class="container">
         <h1>Billentyűzet felvétele</h1><br>
         <form action="felvetel.php" method="post" name="billentyuzet_felvetel" onsubmit="return validalas()">
-            <div>
-                <label for="termek">Termék neve</label>
-                <input type="text" name="termek" id="termek" required>
+            <div class="mb-3">
+                <label class="form-label" for="termek">Termék neve</label>
+                <input class="form-control" type="text" name="termek" id="termek" required>
             </div>
-            <div>
-                <label for="megjelenes_eve">Megjelenés éve</label>
-                <input type="number" name="megjelenes" id="megjelenes" min="1950" max="2022" required>
+            <div class="mb-3">
+                <label class="form-label" for="megjelenes_eve">Megjelenés éve</label>
+                <input class="form-control" type="number" name="megjelenes" id="megjelenes" min="1950" max="2022" required>
             </div>
-            <div>
-                <span>Kapcsolódás</span>
-                <input type="radio" name="kapcsolodas" id="kapcsolodas" value="vezetekes" required>
-                <label for="vezetekes">vezetékes</label>
-                <input type="radio" name="kapcsolodas" id="kapcsolodas" value="vezeteknelkuli">
-                <label for="vezeteknelkuli">vezeték nélküli</label>                
+            <label class="" for="">Kapcsolódás</label><br><br>
+            <div class="mb-3 form-check">                
+                <input class="form-check-input" type="radio" name="kapcsolodas" id="kapcsolodas" value="vezetékes" required>
+                <label class="form-check-label" for="vezetekes">vezetékes</label><br>
+                <input class="form-check-input" type="radio" name="kapcsolodas" id="kapcsolodas" value="vezeték nélküli">
+                <label class="form-check-label" for="vezeteknelkuli">vezeték nélküli</label>                
             </div>
-            <div>
-                <label for="meret">Méret</label>
-                <select name="meret" id="meret" required>
-                    <option value="full_size">Full-size (100%)</option>
-                    <option value="tenkeyless">TenKeyLess (80%)</option>
-                    <option value="compact">Compact (60%)</option>
+            <div class="mb-3">
+                <label class="form-label" for="meret">Méret</label>
+                <select class="form-select" name="meret" id="meret" required>
+                    <option value="full-size 100%">Full-size (100%)</option>
+                    <option value="tenkeyless 80%">TenKeyLess (80%)</option>
+                    <option value="compact 60%">Compact (60%)</option>
                 </select>
             </div>
-            <div>
-                <label for="mechanic">Mechanikus</label>
-                <input type="checkbox" name="mechanik" id="mechanik">
+            <div class="mb-3 form-check">
+                <label class="form-check-label" for="mechanic">Mechanikus</label>                             
+                <input class="form-check-input" type="checkbox" name="mechanik">
             </div>
             <div>
-                <button type="submit">Felvétel</button>
+                <button class="btn btn-primary mb-3" type="submit">Felvétel</button>
             </div>
         </form>
         <?php
@@ -67,26 +75,27 @@
                 }
                 if (!isset($_POST['meret']) || empty($_POST['meret'])) {
                     $hiba .= "A billentyűzet méretét ki kell választani. ";
-            }            
-        ?>
-        <?php if ($hiba == ""): ?>
-        <?php
-            $file = fopen("adatok.csv", "a");
-            $sor = implode(";", $_POST) . PHP_EOL;
-            fwrite($file, $sor);
-        ?>
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            Sikeres felvétel.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <?php else: ?>                
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <?php echo $hiba ?>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <?php endif; ?>
-        <?php
-        }
+                }             
+                $_POST['mechanik'] = isset($_POST['mechanik']) ? "igen" : "nem";                                              
+                ?>
+                <?php if ($hiba == ""): ?>
+                <?php
+                    $file = fopen("adatok.csv", "a");
+                    $sor = implode(";", $_POST) . PHP_EOL;
+                    fwrite($file, $sor);
+                ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    Sikeres felvétel.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php else: ?>                
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?php echo $hiba ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php endif; ?>
+                <?php
+             }
         ?>
     </main>
 </body>
--- index.php
+++ index.php
@@ -1,29 +1,69 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Billentyűzetek</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
-    <nav class="nav nav-pills flex-column flex-sm-row">
-        <a class="flex-sm-fill text-sm-center nav-link active" href="index.php">Listázás</a>
-        <a class="flex-sm-fill text-sm-center nav-link" href="felvetel.php">Felvétel</a>  
+    <nav class="ms-4">
+        <ul class="nav my-3 mx-5 nav-pills">
+            <li class="nav-item">
+                <a class="nav-link"  href="felvetel.php">Felvétel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Listázás</a>
+            </li>   
+        </ul>
     </nav>
     <main class="container">
-        <h1>Billentyűzet</h1>
-        <table>
+        <h1>Billentyűzetek</h1>
+        <table class="table table-striped mt-5">
             <thead>
                 <tr>
                     <th>#</th>
                     <th>Termék</th>
                     <th>Megjelenés éve</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <th>Kapcsolódás</th>
+                    <th>Méret</th>
+                    <th>Mechanikus</th>
                 </tr>
             </thead>
+            <tbody>
+                <?php
+                $file = fopen("adatok.csv", "r");
+                $i = 0;
+                ?>
+                <?php while ($sor = fgets($file)) : ?>
+                    <?php
+                    $i++;
+                    $adatok = explode(';', trim($sor));
+                    ?>
+                    <tr>
+                        <td><?php echo $i ?></td>
+                        <td><?php echo $adatok[0] ?></td>
+                        <td><?php echo $adatok[1] ?></td>
+                        <td><?php echo $adatok[2] ?></td>
+                        <td><?php echo $adatok[3] ?></td>
+                        <td><?php echo $adatok[4] ?></td>
+                    </tr>                
+                <?php endwhile; ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>#</th>
+                    <th>Termék</th>
+                    <th>Megjelenés éve</th>
+                    <th>Kapcsolódás</th>
+                    <th>Méret</th>
+                    <th>Mechanikus</th>
+                </tr>
+            </tfoot>
         </table>
     </main>
 </body>
